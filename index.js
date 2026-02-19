cc7d415a9c17a546dbc64f73a6b61d0dc9bb5ebd
Maxi Baroni <maxi@ejemplo.com>
2026-02-19 18:32:15 -0300

    Cambio a modelo instant estable

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/chat', async (req, res) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-               model:"llama-3.3-70b-specdec",
+               model:"llama-3.1-8b-instant",
                 messages: [
                     { 
                         role: "system", 
